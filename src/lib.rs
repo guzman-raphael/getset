1f65fa1409e1fc3bcc04ac8d81a4886a63b4881e
guzman-raphael <raphael.h.guzman@gmail.com>
2025-04-06 00:55:38 +0000

    Add uniffi attribute.

--- src/lib.rs
+++ src/lib.rs
@@ -355,6 +355,7 @@ fn produce(ast: &DeriveInput, params: &GenParams) -> TokenStream2 {
             let generated = generate::implement_for_unnamed(field, params);
 
             quote! {
+                #[uniffi::export]
                 impl #impl_generics #name #ty_generics #where_clause {
                     #generated
                 }
@@ -363,6 +364,7 @@ fn produce(ast: &DeriveInput, params: &GenParams) -> TokenStream2 {
             let generated = fields.iter().map(|f| generate::implement(f, params));
 
             quote! {
+                #[uniffi::export]
                 impl #impl_generics #name #ty_generics #where_clause {
                     #(#generated)*
                 }
